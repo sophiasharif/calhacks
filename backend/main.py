813c75b32be1e1a222da3bd8af4557321d3de179
priyanshush325 <priyanshush325@gmail.com>
2023-06-17 11:39:03 -0700

    added boilerplate for get and post requests

--- backend/main.py
+++ backend/main.py
@@ -8,7 +8,13 @@ CORS(app)
 def test():
     return "bucks in 6"
 
+@app.route("/get/<query>", methods=['GET'])
+def testGet(query):
+    return ("your query was " + query)
 
+@app.route("/post/<query>", methods=['POST'])
+def testPost(query):
+    return ("big chungus idk bruh")
 
 if __name__ == ("__main__"):
     app.run()
