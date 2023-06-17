4a821380e1fe2589247f42fe68add7ba4ceb3888
priyanshush325 <priyanshush325@gmail.com>
2023-06-17 15:08:29 -0700

    added speech page to react router

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+// import App from "./App.jsx";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import SpeechComponent from "./components/SpeechComponent.jsx";
+import { SpeechPage } from "./pages/SpeechPage.jsx";
 
 const router = createBrowserRouter([
 	{
 		path: "/",
-		element: <App></App>,
+		element: <SpeechPage></SpeechPage>,
 	},
 	{
 		path: "/speech",
