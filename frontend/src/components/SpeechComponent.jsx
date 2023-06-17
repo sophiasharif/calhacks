b2034d2e7d41d267fa550eb9e2be87a5479fcf36
Sonav Agarwal <42980809+SonavAgarwal@users.noreply.github.com>
2023-06-17 15:39:18 -0700

    chunk speech into blocks

--- frontend/src/components/SpeechComponent.jsx
+++ frontend/src/components/SpeechComponent.jsx
@@ -1,16 +1,64 @@
-import React from "react";
+import React, {
+	useCallback,
+	useEffect,
+	useReducer,
+	useRef,
+	useState,
+} from "react";
 import "regenerator-runtime/runtime";
+import { createSpeechlySpeechRecognition } from "@speechly/speech-recognition-polyfill";
 import SpeechRecognition, {
 	useSpeechRecognition,
 } from "react-speech-recognition";
+import { debounce } from "debounce";
+
+// const appId = import.meta.env.VITE_SPEECHLY_APP_ID;
+// const SpeechlySpeechRecognition = createSpeechlySpeechRecognition(appId);
+// SpeechRecognition.applyPolyfill(SpeechlySpeechRecognition);
 
 const SpeechComponent = () => {
+	const [ignored, forceUpdate] = useReducer((x) => x + 1, 0);
+
 	const {
-		transcript,
+		transcript: textStream,
 		listening,
-		resetTranscript,
+		resetTranscript: resetTextStream,
 		browserSupportsSpeechRecognition,
 	} = useSpeechRecognition();
+
+	const transcript = useRef([]);
+	const transcriptLength = useRef(0);
+
+	useEffect(() => {
+		console.log("textStream changed");
+		console.log(textStream);
+	}, [textStream]);
+
+	function updateTranscript(newTextStream) {
+		let newBlock = newTextStream.substring(
+			transcriptLength.current,
+			newTextStream.length
+		);
+		transcript.current.push(newBlock);
+		transcriptLength.current = newTextStream.length;
+
+		forceUpdate();
+		console.log(transcript.current);
+	}
+
+	const updateTranscriptDebounced = useCallback(
+		debounce((newTextStream) => {
+			updateTranscript(newTextStream);
+		}, 1000),
+		[]
+	);
+
+	useEffect(() => {
+		if (textStream.length > transcriptLength.current + 250) {
+			updateTranscript(textStream);
+		}
+		updateTranscriptDebounced(textStream);
+	}, [textStream, transcriptLength, updateTranscriptDebounced]);
 
 	if (!browserSupportsSpeechRecognition) {
 		return <span>Browser doesn't support speech recognition.</span>;
@@ -27,8 +75,11 @@ const SpeechComponent = () => {
 				Start
 			</button>
 			<button onClick={SpeechRecognition.stopListening}>Stop</button>
-			<button onClick={resetTranscript}>Reset</button>
-			<p>{transcript}</p>
+			<button onClick={resetTextStream}>Reset</button>
+			{/* <p>{textStream}</p> */}
+			{transcript.current.map((block, index) => {
+				return <p key={index}>{block}</p>;
+			})}
 		</div>
 	);
 
