844cc5aaab47e7794fb954bf717fdcca2427c611
sophiasharif <57047003+sophiasharif@users.noreply.github.com>
2023-06-18 00:30:51 -0700

    sources sourcing done

--- frontend/src/helpers/GPTEndpoints.js
+++ frontend/src/helpers/GPTEndpoints.js
@@ -1,24 +1,48 @@
+import factCheckClaim from "./factCheckClaim";
 import generateChatGPTResponse from "./generateGPTResponse";
-import { factCheckPrompt, suggestionPrompt, summarizePrompt } from "./prompts";
+import {
+  factCheckPrompt,
+  suggestionPrompt,
+  summarizePrompt,
+  getFactsPrompt,
+} from "./prompts";
 
 export async function factCheck(text) {
-	const response = JSON.parse(
-		await generateChatGPTResponse(factCheckPrompt, text)
-	);
+  const response = JSON.parse(
+    await generateChatGPTResponse(factCheckPrompt, text)
+  );
 
-	return response;
+  return response;
 }
 
 export async function suggestAnswers(text) {
-	const response = JSON.parse(
-		await generateChatGPTResponse(suggestionPrompt, text)
-	);
+  const response = JSON.parse(
+    await generateChatGPTResponse(suggestionPrompt, text)
+  );
 
-	return response;
+  return response;
 }
 
 export async function summarizeLessonPlan(text) {
-	const response = await generateChatGPTResponse(summarizePrompt, text);
+  const response = await generateChatGPTResponse(summarizePrompt, text);
 
-	return response;
+  return response;
+}
+
+export async function getContradictingSources(text) {
+  // works but is very slow!
+
+  const claims = JSON.parse(
+    await generateChatGPTResponse(getFactsPrompt, text)
+  )["claims"];
+
+  let sources = [];
+  for (let i = 0; i < claims.length; i++) {
+    const claimCheck = await factCheckClaim(claims[i]);
+    console.log(claimCheck); // print every claim it sends a request to
+    sources.push(claimCheck.article);
+  }
+
+  console.log(sources); // prints all the sources at the end
+  return sources;
 }
--- frontend/src/helpers/prompts.js
+++ frontend/src/helpers/prompts.js
@@ -1,10 +1,13 @@
 const factCheckPrompt =
-	"Do not respond with anything except for a JSON object. The JSON object should contain the following fields: \n1) factual: If everything in the paragraph below is accurate, set the value of factual to true. If the paragraph below contains inaccuracies, set factual to false. \n2) unclear: If the statement is too vague or short to evaluate, set unclear to true. If it is not possible to determine if there are inaccuracies, set unclear to true. \n3) corrections: If factual is false, set corrections to an array of one sentence long strings describing the corrections to the paragraph. \n\nHere is the text you should fact check:";
+  "Do not respond with anything except for a JSON object. The JSON object should contain the following fields: \n1) factual: If everything in the paragraph below is accurate, set the value of factual to true. If the paragraph below contains inaccuracies, set factual to false. \n2) unclear: If the statement is too vague or short to evaluate, set unclear to true. If it is not possible to determine if there are inaccuracies, set unclear to true. \n3) corrections: If factual is false, set corrections to an array of one sentence long strings describing the corrections to the paragraph. \n\nHere is the text you should fact check:";
 
 const suggestionPrompt =
-	"Do not respond with anything except for a JSON object, which has a field containsQuestion. If the text below contains a question, do the following three things: 1) set containsQuestion to true. 2) add a field called question with the question in the text in proper english. 3) add a field called answer, with the answer to the question. Here is the text you should analyze: ";
+  "Do not respond with anything except for a JSON object, which has a field containsQuestion. If the text below contains a question, do the following three things: 1) set containsQuestion to true. 2) add a field called question with the question in the text in proper english. 3) add a field called answer, with the answer to the question. Here is the text you should analyze: ";
 
 const summarizePrompt =
-	"Summarize the lesson plan provided below. The intent is for the summary to be provided as context before a question relating to the lesson is asked.  Do not respond with anything except for a 1-2 sentence summary.";
+  "Summarize the lesson plan provided below. The intent is for the summary to be provided as context before a question relating to the lesson is asked.  Do not respond with anything except for a 1-2 sentence summary.";
 
-export { factCheckPrompt, suggestionPrompt, summarizePrompt };
+const getFactsPrompt =
+  "List all the claims made in the text provided below. Do not respond with anything except for a JSON object with a claims field that is an array of claims made in the text. Each claim should be stand-alone and comprehensible when read on its own. Here is the text:";
+
+export { factCheckPrompt, suggestionPrompt, summarizePrompt, getFactsPrompt };
